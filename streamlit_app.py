5c70d3841204e9c62b0c26aaf081e3e188d56348
Manojsgowda789 <124457331+Manojsgowda789@users.noreply.github.com>
2023-05-16 10:20:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 avocado toast')
 
 streamlit.header('breakfast favorites')
-streamlit.header(' Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
